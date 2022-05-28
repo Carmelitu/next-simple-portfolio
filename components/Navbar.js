69330cd0f9f5acfae7fb9597e411dd4dffeebbf7
Lucas Seijo <lucaseijo7@gmail.com>
2022-05-28 15:44:35 -0300

    remuevo toggler de navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,10 +6,7 @@ const Navbar = () => (
       <Link href="/">
         <a className="navbar-brand" href="#">Portfolio</a>
       </Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div id="navbarNav">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item d-inline-flex">
             <div className="nav-link">
